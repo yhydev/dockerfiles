8115cdd15dd042e5b6d59d594eb298706d5a4602
yhydev <yhydev@foxmail.com>
2019-01-26 00:14:21 +0800

    update mysql docker

--- mysql/start.sh
+++ mysql/start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker run --name mysql -p 3306:3306 -v /etc/localtime:/etc/localtime -v /root/mysql-data:/var/lib/mysql -v /root/mysql-conf/:/etc/mysql -e MYSQL_ROOT_PASSWORD=mysqlpasswd -d  daocloud.io/library/mysql
+docker rm -f  mysql
+rm -rf /root/mysql-conf /root/mysql-data
+#mkdir /root/mysql-conf /root/mysql-data
+#chmod 777 -R /root/mysql-conf /root/mysql-data
+
+docker run --name mysql  -p 3306:3306 -v /etc/localtime:/etc/localtime -v /root/mysql-conf:/etc/mysql/conf.d -v /root/mysql-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456hy -d  daocloud.io/library/mysql
